1d03cb2ecf191caac87ff3e2e9fff1df848d7a09
Xin-Xin MA <xxmawhu@163.com>
2024-07-25 23:18:16 +0800

    update

--- task.sh
+++ task.sh
@@ -2,8 +2,5 @@
 PYTHON="/miniconda/bin/python3.9"
 current_dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 cd ${current_dir}
-echo "[$(date)] begin ..."
-
 mkdir -p log
 taskset -c 1 $PYTHON -Wignore static_notification_forward_bot.py
-echo "[$(date)] query success!"
